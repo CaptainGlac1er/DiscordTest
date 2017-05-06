0ce0c6240a6a205535155821abf91ff0c0a727cf
CaptainGlac1er <georgecolgrove@outlook.com>
2017-05-06 01:11:02 -0400

    minor change

--- gwcImgurConnect/ImgurAPI.cs
+++ gwcImgurConnect/ImgurAPI.cs
@@ -30,9 +30,7 @@ namespace gwcImgurConnect
         {
             try
             {
-                Dictionary<string, string> headers = new Dictionary<string, string>();
-                headers.Add("Authorization", "Bearer " + connectionToken.ImgurConectToken);
-                string json = webAccess.queryWebsiteGET("https://api.imgur.com/3/gallery/search/?q_any=" + search, headers);
+                string json = webAccess.queryWebsiteGET("https://api.imgur.com/3/gallery/search/?q_any=" + search, createHeader());
                 return JsonConvert.DeserializeObject<gallery>(json).data.ToList();
             }
             catch (WebException e)
@@ -49,12 +47,17 @@ namespace gwcImgurConnect
         }
         public ImgurInfo refreshToken()
         {
-            Dictionary<string, string> headers = new Dictionary<string, string>();
-            headers.Add("Authorization", "Bearer " + connectionToken.ImgurConectToken);
-            string json = webAccess.queryWebsitePOST("https://api.imgur.com/oauth2/token", headers, "grant_type=refresh_token&client_id=" + connectionToken.ClientID + "&client_secret=" + connectionToken.ImgurSecret + "&refresh_token=" + connectionToken.ImgurRefreshToken);
+            string json = webAccess.queryWebsitePOST("https://api.imgur.com/oauth2/token", createHeader(), "grant_type=refresh_token&client_id=" + connectionToken.ClientID + "&client_secret=" + connectionToken.ImgurSecret + "&refresh_token=" + connectionToken.ImgurRefreshToken);
             refreshJSON newInfo = JsonConvert.DeserializeObject<refreshJSON>(json);
             connectionToken = new ImgurInfo(newInfo.access_token, newInfo.refresh_token, connectionToken.ClientID, connectionToken.ImgurSecret);
             return connectionToken;
+        }
+        private Dictionary<string, string> createHeader()
+        {
+            Dictionary<string, string> headers = new Dictionary<string, string>();
+            headers.Add("Authorization", "Bearer " + connectionToken.ImgurConectToken);
+            return headers;
+
         }
     }
 }
